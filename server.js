0af79d5dd84abb582988165495cd9485b4373c63
kokkoon <kkgan0930@gmail.com>
2020-10-02 01:03:35 +0800

    tested locally with server

--- server.js
+++ server.js
@@ -1,4 +1,48 @@
 const express = require('express');
-const Bull = require('Bull');
+const Bull = require('bull');
 const GUI = require('bull-arena');
 const keys = require('./config/keys');
+
+const PORT = '5005'; //process.env.PORT || '5001';
+const REDIS_URL = process.env.REDIS_URL || keys.redisURL
+
+const queueDashboard = GUI({
+    Bull,
+	queues: [
+		{
+			name: "FLOW",
+			hostId: "flow",
+			url: keys.redisURL 
+		},
+		{
+			name: "SERVICE",
+			hostId: "flow",
+			url: keys.redisURL 
+		},
+		{
+			name: "MESSAGE",
+			hostId: "flow",
+			url: keys.redisURL 
+		},
+		{
+			name: "SCHEDULE",
+			hostId: "flow",
+			url: keys.redisURL 
+		}
+	]
+}, {
+	basePath: "/",
+	disableListen: true
+});
+
+let app = express();
+
+app.use('/queue_dashboard', queueDashboard);
+
+let scheduleQueue = new Bull('SCHEDULE', REDIS_URL);
+
+scheduleQueue.on('global:completed', (jobId, result) => {
+    console.log(`Job completed with result ${result}`);
+  });
+  
+app.listen(PORT, () => console.log("Server started!"));
