86867d48acdfd5dccd53167f3e884dddf9a4321e
kokkoon <kkgan0930@gmail.com>
2020-11-05 17:06:04 +0800

    update

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -131,7 +131,7 @@ module.exports = app => {
 
 	  resQueue.getJobs(['waiting'], 0, 100)
 	  	.then(async result => {
-			  console.log(result)
+			if (result.length<1) return `There was no pending task for you`;
 			var waitingJob = result.filter(obj => {return obj.data.to === req.body.From})
 			console.log(`# of waiting jobs for ${req.body.From}`, waitingJob.length)
 			const outcome = msg.match(/Approve/i) ? 'approved': msg.match(/Reject/i) ? 'rejected':undefined;
