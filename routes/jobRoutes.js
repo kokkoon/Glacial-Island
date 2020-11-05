870d31a347eddf84096aa00f47c3aacdcdd7ffdb
kokkoon <kkgan0930@gmail.com>
2020-11-05 12:23:09 +0800

    update

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -177,7 +177,7 @@ const resume = async (jobId, outcome) => {
 	const job = await flowQueue.getJob(jobId);
 	console.log(jobId, job.data.state)
 	if (job.data.state !== "Paused") {
-		//res.send("Only a paused job could be resumed");
+		console.log("Only a paused job could be resumed");
 		return false;
 	}
 	const jobData = {...job.data};
