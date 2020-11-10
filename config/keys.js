a5b0099633bfcfe0c1833caf2ff06187947f1fc2
kokkoon <kkgan0930@gmail.com>
2020-11-10 15:39:55 +0800

    added auth

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
+    PortalHost : 'https://portal.flowngin.com/api',
     //redisURL: 'redis://h:zwWbvx0uyH2ZYceqMAUzeHXm8u90ROnK@redis-13053.c1.asia-northeast1-1.gce.cloud.redislabs.com:13053',
     redisURL: 'redis://h:9kcYe9Z2721c@104-238-212-163.cloud-xip.io:6379',
     redisHost: '104-238-212-163.cloud-xip.io', //redis-13053.c1.asia-northeast1-1.gce.cloud.redislabs.com',
