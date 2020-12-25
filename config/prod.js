caf98f9c5c334e45030884efa61425b57bd0c8ad
kokkoon <kkgan0930@gmail.com>
2020-12-25 09:03:41 +0800

    added config for dev and local

---
+++ config/prod.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PortalHost : 'https://portal.glozic.com/api',
+    redisURL: 'redis://h:9kcYe9Z2721c@104-238-212-163.cloud-xip.io:6379',
+    redisHost: '104-238-212-163.cloud-xip.io',
+    redisPort: '6379',
+    redisPWD: '9kcYe9Z2721c', 
+    twilioAccountSid: 'AC9905994c6e25ef3ed2c3b4e57cbda227',
+    twilioAuthToken: '81cd983cd4e6c04426f95138716d0a49',
+  };
