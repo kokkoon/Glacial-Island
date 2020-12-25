d4a96320eb734cc0421c9da44b480e101d9889ea
kokkoon <kkgan0930@gmail.com>
2020-12-25 16:32:04 +0800

    update redi config

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     PortalHost : 'https://portal.glozic.com/api',
-    redisURL: 'redis://h:9kcYe9Z2721c@104-238-212-163.cloud-xip.io:6379',
+    redisURL: 'redis://h:9kcYe9Z2721c@104.238.212.163:6379',
     redisHost: '104-238-212-163.cloud-xip.io',
     redisPort: '6379',
     redisPWD: '9kcYe9Z2721c', 
