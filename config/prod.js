6f8ef0dd0d6a6cb614f329d903cc34dede502ff7
kokkoon <kkgan0930@gmail.com>
2020-12-26 04:07:38 +0800

    update redi config

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,7 @@
 module.exports = {
     PortalHost : 'https://portal.glozic.com/api',
     redisURL: 'redis://h:9kcYe9Z2721c@104.238.212.163:6379',
-    redisHost: '104-238-212-163.cloud-xip.io',
+    redisHost: '104.238.212.163', //'104-238-212-163.cloud-xip.io',
     redisPort: '6379',
     redisPWD: '9kcYe9Z2721c', 
     twilioAccountSid: 'AC9905994c6e25ef3ed2c3b4e57cbda227',
