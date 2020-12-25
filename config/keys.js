893ed7d86859095f920358e1dc4f7f6d868df729
kokkoon <kkgan0930@gmail.com>
2020-12-25 09:42:59 +0800

    fixed ts issue

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+console.log("NODE_ENV: " + process.env.NODE_ENV);
+var keys;
+switch (process.env.NODE_ENV) {
+  case 'production':
+    keys = require('./prod');
+    break;
+  case 'development':
+    keys = require('./dev');
+    break;
+  case 'local':
+    keys = require('./local');
+    break;
+}
+
+export default keys
