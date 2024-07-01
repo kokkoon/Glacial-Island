a5002c7f626e84b677b5b31a63838f67164b6422
Anup Maurya <anup.maurya484@ggmaill.com>
2024-07-01 11:56:42 +0530

    Fixed issue

--- index.js
+++ index.js
@@ -172,8 +172,8 @@ const imagesDir = path.join(__dirname, 'images');
 require('./routes/jobRoutes')(app);
 
 
-console.log(process.env.PORT);
-const PORT = 433;
+
+const PORT = process.env.PORT || '4000';
 app.listen(PORT, '0.0.0.0', () => {
 	console.log(`Server is running on port ${PORT}`)
 })
