e98ab27464b2ef024c8dfb1266ca059ad813ed61
Anup Maurya <anup.maurya484@gmail.com>
2026-07-15 13:50:45 +0530

    Add Logs

--- index.js
+++ index.js
@@ -96,5 +96,6 @@ require('./routes/jobRoutes')(app);
 
 const PORT = process.env.PORT || '4000';
 app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Start ${process.env.NODE_ENV}`)
 	console.log(`Server is running on port ${PORT}`)
 })
